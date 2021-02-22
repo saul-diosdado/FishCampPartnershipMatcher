cbed71078c8148d4fb01bc92a2d5ba187a41f3f9
Saul Diosdado <sdbsns825@gmail.com>
2021-02-21 18:16:04 -0600

    Migrate DB to version 0.

--- db/schema.rb
+++ db/schema.rb
@@ -10,46 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_022753) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "answers", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "question_id"
-    t.text "short_answer"
-    t.boolean "true_false"
-    t.integer "numeric"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
-    t.index ["user_id"], name: "index_answers_on_user_id"
-  end
-
-  create_table "choices", force: :cascade do |t|
-    t.text "content"
-    t.bigint "question_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id"], name: "index_choices_on_question_id"
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.text "question"
-    t.string "question_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
-    t.string "role"
-    t.boolean "approved"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "choices", "questions"
 end

f347bb24fe17457cde7b4a32551857195c812c37
Saul Diosdado <sdbsns825@gmail.com>
2021-02-21 22:50:13 -0600

    Questions on the preference form.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
         render(:partial => "application/error_messages", :locals => {:object => object}) 
     end
      
+    def get_partial(question)
+        if question.question_type == "True/False"
+            "true_false"
+        elsif question.question_type == "Numeric"
+            "numeric"
+        elsif question.question_type == "Multiple Choice"
+            "multiple_choice"
+        else
+            "short_answer"
+        end
+    end
+
 end

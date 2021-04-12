c83254114a54163aaa4a5dd1a185c7224e254ba5
ahsanagwan <agwanahsan@gmail.com>
2021-04-11 20:52:15 -0500

    Update application_controller.rb
    adding the authentication process, must create a user before hand to have a role of admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@
 
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+  
+  
+  module Admin
+    class ApplicationController < Administrate::ApplicationController
+      before_action :authenticate_user!
+      before_action :authenticate_admin
+      def authenticate_admin
+        redirect_to '/', alert: 'Not authorized.' unless current_user && access?
+      end
+      private
+      def access?
+        current_user.has_role? :admin
+      end
+    end
+  end
 end
